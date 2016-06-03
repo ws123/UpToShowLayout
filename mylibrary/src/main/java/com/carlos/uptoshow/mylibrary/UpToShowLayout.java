3b1055ef456f6d0bd741bfb58f3e5ce9a132f24c
carlos <372532647@qq.com>
2016-06-03 14:58:04 +0800

    增加了下拉刷新

--- mylibrary/src/main/java/com/carlos/uptoshow/mylibrary/UpToShowLayout.java
+++ mylibrary/src/main/java/com/carlos/uptoshow/mylibrary/UpToShowLayout.java
@@ -17,6 +17,8 @@ public class UpToShowLayout extends RelativeLayout {
     private int translateY = 0;
     //第二个view是否显示中
     private boolean isSecondShow = false;
+    private boolean isTimeToRefresh = false;
+    private boolean isRefreshing = false;
 
     private ViewChangeListener viewChangeListener;
 
@@ -56,6 +58,7 @@ public class UpToShowLayout extends RelativeLayout {
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (isRefreshing) return super.onInterceptTouchEvent(ev);
         if (isSecondShow) {
             if (!ViewCompat.canScrollVertically(getChildAt(1), -1)) {
                 switch (ev.getAction()) {
@@ -97,6 +100,25 @@ public class UpToShowLayout extends RelativeLayout {
                         translateY = 0;
                         break;
                 }
+            } else if (!ViewCompat.canScrollVertically(getChildAt(0), -1)) {
+                switch (ev.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        translateY = 0;
+                        lastMoveY = (int) ev.getY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        if (lastMoveY < ev.getY()) {
+                            isTimeToRefresh = true;
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    case MotionEvent.ACTION_UP:
+                        isTimeToRefresh = false;
+                        lastMoveY = 0;
+                        translateY = 0;
+                        break;
+                }
             }
         }
         return super.onInterceptTouchEvent(ev);
@@ -112,7 +134,11 @@ public class UpToShowLayout extends RelativeLayout {
         if (isSecondShow) {
             return secondViewTouchEvent(event);
         } else {
-            return firstViewTouchEvent(event);
+            if (isTimeToRefresh) {
+                return pullToRefresh(event);
+            } else {
+                return firstViewTouchEvent(event);
+            }
         }
     }
 
@@ -148,6 +174,50 @@ public class UpToShowLayout extends RelativeLayout {
                     isSecondShow = false;
                     if (viewChangeListener != null) viewChangeListener.hideAfterAnim();
                 }
+                translateY = 0;
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    private boolean pullToRefresh(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                translateY = (int) (event.getY() - lastMoveY) / 3;
+                if (translateY < 0) {
+                    translateY = 0;
+                    return super.onTouchEvent(event);
+                }
+                getChildAt(0).setTranslationY(translateY);
+                break;
+            case MotionEvent.ACTION_UP:
+                isTimeToRefresh = false;
+                if (getChildAt(0).getTranslationY() > 200) {
+                    isRefreshing = true;
+                    //达到了下拉刷新的标准
+                    ValueAnimator animator = ValueAnimator.ofFloat(getChildAt(0).getTranslationY(), 200);
+                    animator.setTarget(getChildAt(0));
+                    animator.setDuration((long) ((getChildAt(0).getTranslationY() - 200) * 2)).start();
+                    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator animation) {
+                            getChildAt(0).setTranslationY((Float) animation.getAnimatedValue());
+                        }
+                    });
+                } else {
+                    ValueAnimator animator = ValueAnimator.ofFloat(getChildAt(0).getTranslationY(), 0);
+                    animator.setTarget(getChildAt(0));
+                    animator.setDuration((long) (getChildAt(0).getTranslationY() * 2)).start();
+                    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator animation) {
+                            getChildAt(0).setTranslationY((Float) animation.getAnimatedValue());
+                        }
+                    });
+                }
+                translateY = 0;
+                break;
+            case MotionEvent.ACTION_CANCEL:
                 translateY = 0;
                 break;
         }
@@ -194,6 +264,19 @@ public class UpToShowLayout extends RelativeLayout {
 
     public void setViewChangeListener(ViewChangeListener viewChangeListener) {
         this.viewChangeListener = viewChangeListener;
+    }
+
+    public void stopRefreshing() {
+        ValueAnimator animator = ValueAnimator.ofFloat(getChildAt(0).getTranslationY(), 0);
+        animator.setTarget(getChildAt(0));
+        animator.setDuration((long) (getChildAt(0).getTranslationY() * 2)).start();
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                getChildAt(0).setTranslationY((Float) animation.getAnimatedValue());
+            }
+        });
+        isRefreshing = false;
     }
 
     interface ViewChangeListener {
